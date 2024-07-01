b80cb539f70b93a68bffde380b59e59da5b76a0a
kousei ikuta <e1n21009@oit.ac.jp>
2024-07-01 10:38:48 +0900

    calc

--- app/src/main/java/org/example/Calculate.java
+++ app/src/main/java/org/example/Calculate.java
@@ -4,4 +4,62 @@ public class Calculate {
   public int sum(int x, int y) {
     return x + y;
   }
+
+  public int sumRange(int start, int end) {
+    int sum = 0;
+    for (int i = start; i <= end; i++) {
+      sum += i;
+    }
+    return sum;
+  }
+
+  public double average(int total, int count) {
+    return (double) total / count;
+  }
+
+  public int sumOddRange(int start, int end) {
+    int sum = 0;
+    for (int i = start; i <= end; i++) {
+      if (i % 2 != 0) {
+        sum += i;
+      }
+    }
+    return sum;
+  }
+
+  public int sumEvenRange(int start, int end) {
+    int sum = 0;
+    for (int i = start; i <= end; i++) {
+      if (i % 2 == 0) {
+        sum += i;
+      }
+    }
+    return sum;
+  }
+
+  public String getSumAndAverageOf2And3() {
+    int sum = sum(2, 3);
+    double average = average(sum, 2);
+    return "Sum of 2 and 3 is " + sum + ". Average is " + average + ".";
+  }
+
+  public String getSumAndAverageOf1To10() {
+    int sum = sumRange(1, 10);
+    double average = average(sum, 10);
+    return "Sum of 1 to 10 is " + sum + ". Average is " + average + ".";
+  }
+
+  public String getSumOfOddAndEvenOf1To10() {
+    int sumOdd = sumOddRange(1, 10);
+    int sumEven = sumEvenRange(1, 10);
+    return "Sum of odd of 1 to 10 is " + sumOdd + ". Sum of even is " + sumEven + ".";
+  }
+
+  public static void main(String[] args) {
+    Calculate calc = new Calculate();
+
+    System.out.println(calc.getSumAndAverageOf2And3());
+    System.out.println(calc.getSumAndAverageOf1To10());
+    System.out.println(calc.getSumOfOddAndEvenOf1To10());
+  }
 }
--- app/src/test/java/org/example/CalculateTest.java
+++ app/src/test/java/org/example/CalculateTest.java
@@ -8,7 +8,8 @@ public class CalculateTest {
   @Test
   public void testSum() {
     Calculate calculate = new Calculate();
-    int expected = 5;
-    assertEquals(expected, calculate.sum(2, 3));
+    assertEquals("Sum of 2 and 3 is 5. Average is 2.5.", calculate.getSumAndAverageOf2And3());
+    assertEquals("Sum of 1 to 10 is 55. Average is 5.5.", calculate.getSumAndAverageOf1To10());
+    assertEquals("Sum of odd of 1 to 10 is 25. Sum of even is 30.", calculate.getSumOfOddAndEvenOf1To10());
   }
 }
